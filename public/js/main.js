92312707c3b2d533a67727b823b485ca3a3256f8
LAPTOP-J6F0OR4J\TSelmeg <99040437@mydavinci.nl>
2018-09-21 10:16:13 +0200

    merging

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,7 @@ function Model ()
 {
     var base_url = window.location.origin;
     var self = this
+    self.files = ko.observableArray()
     self.loginButton = ko.observable()
     self.loginInfo = ko.observableArray([
         {name:"email"},
@@ -30,6 +31,58 @@ function Model ()
     self.user = ko.observableArray()
     self.userDevice = ko.observableArray()
     self.deviceMeter = ko.observableArray()
+
+    /**
+    *
+    *  canvas coordinates
+    */
+    var canvas = document.getElementById('background')
+    var context = canvas.getContext('2d')
+    $('#background').click(function(event)
+    {
+        let xy = getMousePos(canvas,event)
+        console.log(getMousePos(canvas,event))
+        
+        context.fillStyle = "#FF0000";  
+        context.fillRect(xy['x'],xy['y'],10,10)
+    })
+    function getMousePos(canvas, event) {
+        var rect = canvas.getBoundingClientRect()-
+        return {
+            x: event.clientX - rect.left,
+            y: event.clientY - rect.top
+            }
+    }
+    /**
+    *
+    *   upload image
+    */
+        self.fileSelect= function (element,event) {
+        var files =  event.target.files;// FileList object
+        // Loop through the FileList and render image files as thumbnails.
+        for (var i = 0, f; f = files[i]; i++) {
+
+          // Only process image files.
+            if (!f.type.match('image.*')) {
+                continue;
+            }          
+
+            var reader = new FileReader();
+
+              // Closure to capture the file information.
+            reader.onload = (function() {
+                return function(event){
+                    var img = new Image();
+                    img.addEventListener("load", function() {
+                    context.drawImage(img, 0, 0);
+                    });
+                    img.src = event.target.result;
+                }                        
+            })(f);
+              // Read in the image file as a data URL.
+            reader.readAsDataURL(f);
+        }
+    };
 
     /**
      * [loginToken description]
