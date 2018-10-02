f8893b978823f2986eb4e2498987f96502d1ddd9
LAPTOP-J6F0OR4J\TSelmeg <99040437@mydavinci.nl>
2018-10-02 16:41:55 +0200

    new begin

--- public/js/main.js
+++ public/js/main.js
@@ -2,32 +2,18 @@ $.ajaxSetup({
   headers: {
     'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
   }
-});
+})
+var localStorage = window.localStorage
+var base_url = window.location.origin
 /**
 *
 *   Knockoutjs
 */
 var ViewModel = function (){
-  var localStorage = window.localStorage;
-  var base_url = window.location.origin;
+  
   var self = this
   self.files = ko.observableArray()
   self.none = ko.observable('none')
-
-  self.loginButton = ko.observable()
-  self.loginInfo = ko.observableArray([
-    {name:"email"},
-    {name:"password"}
-  ])
-  self.registerInfo = ko.observableArray([
-    {name:"email"},
-    {name:"name"},
-    {name:"password"}
-  ])
-  self.forgetInfo = ko.observableArray([
-    {name:"email"}
-  ])
-
   self.profiles = ko.observableArray([
     {name:"Name"},
     {name:"Email"}
@@ -60,7 +46,7 @@ var ViewModel = function (){
     {name:"Ozone"},
     {name:"NO2"},
   ])
-  self.token = ko.observable()
+  
   self.currentPage = ko.observable()
   self.currentTab = ko.observable()
   self.lastCurrentTab = ko.observable()
@@ -170,72 +156,7 @@ var ViewModel = function (){
       reader.readAsDataURL(f);    
 
     }
-  };
-
-  /**
-  * [choosePage description]
-  * @param  {[type]} data [description]
-  * @return {[type]}      [description]
-  */
-  self.choosePage = function(data)
-  {
-    data = data.toLowerCase()
-    self.currentPage(data)
-
-    if (data == "login") {
-      self.loginButton('Sign in')
-      self.currentPageData(self.loginInfo())
-      self.pages([{name: 'Register'}, {name: 'Forgot password'}])
-    }
-    else if (data == "register"){
-      self.loginButton('Sign up')
-      self.currentPageData(self.registerInfo())
-      self.pages([{name: 'Login'}, {name: 'Forgot password'}])
-    }
-    else{
-        self.loginButton('Send email')
-        self.currentPageData(self.forgetInfo())
-        self.pages([{name: 'Register'}, {name: 'Login'}])
-    }
   }
-    
-    /**
-     * [loginToken description]
-     * @return {[type]} [description]
-     */
-    self.loginToken = function() {
-        if (self.loginButton() == "Sign in") {
-            $.post(base_url + '/api/login',{email:$('#email').val(), password:$('#password').val()}).done(function(data)
-            {
-                self.token(data['access_token'])
-                localStorage.setItem('token',self.token())
-                $.post(base_url + '/api/me', {token:self.token()}).done(function(data)
-                {
-                    self.user(data['name'])
-                    if ( data['role'] == 1 ) {
-                      self.userRole('user')
-                      $('.admin').addClass("d-none")     
-                    }
-                    else if ( data['role'] == 2 ) {
-                      self.userRole('admin')
-                      $('#uploadForm').addClass("d-none")
-                    }
-                })
-
-                $("#container").removeClass("d-none")
-                $("#loginCont").addClass("d-none")            
-
-                self.getOrganizations()
-            })
-        } else if (self.loginButton() == "Sign up") {
-            $.post(base_url + '/api/create',{email:$('#email').val(), password:$('#password').val(), name:$('#name').val()}).done(function(data)
-            {
-                self.loginButton("Sign in")
-                self.loginToken()
-            })          
-        }
-    }
-
     /**
      * [logout description]
      * @return {[type]} [description]
@@ -449,29 +370,6 @@ var ViewModel = function (){
     $('#openBtn').click(function(){
         $('#myModal').modal({show:true})
     });
-    self.enterPage = function() {
-        
-        if (localStorage.getItem('token'))
-        {
-            self.token(localStorage.getItem('token'))
-            $.post(base_url + '/api/me', {token: self.token()}).done(function(data){
-                self.user(data['name'])
-                if ( data['role'] == 1 ) {
-                  self.userRole('user')
-                  $('.admin').addClass("d-none")     
-                }
-                else if ( data['role'] == 2 ) {
-                  self.userRole('admin')
-                  $('#uploadForm').addClass("d-none")
-                }
-                $("#container").removeClass("d-none")
-                $("#loginCont").addClass("d-none")    
-                self.getOrganizations()        
-            }).fail()
-        }
-        self.choosePage('login')
-    }
-    self.enterPage()
 
     /**
      * [saveToPhp description]
@@ -484,7 +382,3 @@ var ViewModel = function (){
         });
     };
 }
-
-var vm = new ViewModel();
-ko.applyBindings(vm);
-
