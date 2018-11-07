c4d9c6ae276a637d0483b20cd2794398cc90597c
LAPTOP-J6F0OR4J\TSelmeg <99040437@mydavinci.nl>
2018-11-07 11:20:07 +0100

    seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('test'),
+        ]);
+
+    }
+}
